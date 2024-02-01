29a370e34c891c327534c3de0035188f4ddeff1e
abdalrhmanreda <abdalrhmanr444@gmail.com>
2024-02-01 03:43:09 +0200

    finish alter that contain garage data

--- lib/ui/user/features/maps/home/presentation/components/alert_contain_garage_data.dart
+++ lib/ui/user/features/maps/home/presentation/components/alert_contain_garage_data.dart
@@ -3,6 +3,11 @@ import 'package:easy_parking_app/generated/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iconly/iconly.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+import '../../../../../../../generated/l10n.dart';
+import 'alert_components/details_item.dart';
+import 'alert_components/garage_name_and_rate.dart';
 
 Future<void> showMyDialog(BuildContext context) async {
   return showDialog<void>(
@@ -18,7 +23,7 @@ Future<void> showMyDialog(BuildContext context) async {
         content: SizedBox(
           height: AppConstant.deviceHeight(context) / 3,
           child: Column(
-            children: <Widget>[
+            children: [
               Container(
                 clipBehavior: Clip.antiAliasWithSaveLayer,
                 decoration: BoxDecoration(
@@ -27,7 +32,31 @@ Future<void> showMyDialog(BuildContext context) async {
                     topRight: Radius.circular(20.r),
                   ),
                 ),
-                child: Image.asset(Assets.testModelGarage),
+                child: Image.asset(Assets.testPCPPhoto3sizedforWeb),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: GarageNameAndRate(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  DetailsItem(
+                    title: S.of(context).distance,
+                    subtitle: '12 km',
+                    icon: IconlyBroken.location,
+                  ),
+                  DetailsItem(
+                    title: S.of(context).duration,
+                    subtitle: '5 min',
+                    icon: IconlyBroken.time_circle,
+                  ),
+                  DetailsItem(
+                    title: S.of(context).cost,
+                    subtitle: '120',
+                    icon: Iconsax.dollar_circle,
+                  ),
+                ],
               ),
             ],
           ),
@@ -35,7 +64,9 @@ Future<void> showMyDialog(BuildContext context) async {
         actions: [
           Center(
             child: InkWell(
-              onTap: () {},
+              onTap: () {
+                Navigator.of(context).pop();
+              },
               child: const CircleAvatar(
                 radius: 25,
                 child: Icon(IconlyBroken.arrow_left_2),
